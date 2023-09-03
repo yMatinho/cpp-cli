72cbbc2530ae927130f3f93b8a3ed96d23bc6322
Matheus Acioli <matheusmaceio2013@gmail.com>
2023-09-03 00:03:41 -0300

    feat: Input singleton

---
+++ src/app/input/Input.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "string"
+#include "iostream"
+#include "stdio.h"
+
+using namespace std;
+namespace App
+{
+    namespace Input
+    {
+        class Input
+        {
+        private:
+            Input()
+            {
+            }
+
+        public:
+            static Input *get();
+
+            string getString(string label);
+
+            int getInt(string label);
+            float getFloat(string label);
+            void anyKey(string label = "\n\nDigite qualquer tecla para voltar...");
+            bool wishToContinue(string label = "\n\nDeseja continuar? Y[es] N[o] \n\n");
+        };
+    }
+}
