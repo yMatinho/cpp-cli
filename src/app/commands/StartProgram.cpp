09e802265feced99086905eb6fe66bbe18e86284
Matheus Acioli <matheusmaceio2013@gmail.com>
2023-09-03 07:28:14 -0300

    feat: Execution time

--- src/app/commands/StartProgram.cpp
+++ src/app/commands/StartProgram.cpp
@@ -2,27 +2,38 @@
 #include "../exceptions/Exception.h"
 #include "../exceptions/BreakException.h"
 #include <iostream>
+#include "chrono"
 #include "string"
 #include "../../routes/Router.h"
+#include "iomanip"
 
 using namespace std;
 using namespace Exceptions;
 using namespace Commands;
 using namespace Routes;
 
-void StartProgram::start(int argc, char *argv[])
+void StartProgram::start(int argc, char *argv[], bool executionTime)
 {
+    auto start = std::chrono::high_resolution_clock::now();
+
     try
     {
+
         Router::get()->setParams(argc, argv);
         Router::get()->executeRoute();
     }
     catch (BreakException *e)
     {
-        cout << "Breaking the execution..." << "\n\n";
+        cout << "Breaking the execution..."
+             << "\n\n";
     }
     catch (Exception *e)
     {
         cout << e->getMessage() << "\n\n";
     }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    if (executionTime)
+        std::cout << "\n\n\nExecution time: " << fixed << setprecision(10) << (float)duration.count() / 1000000 << " milliseconds\n\n";
 }
