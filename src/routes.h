8b8522a4c219865c57aa3710a66aefdfbe7afb27
Matheus Acioli <matheusmaceio2013@gmail.com>
2023-09-03 04:16:36 -0300

    feat: ROT13 Encrypt

--- src/routes.h
+++ src/routes.h
@@ -1,6 +1,7 @@
 #include "routes/Router.h"
 #include "app/controllers/GradesController.h"
 #include "app/controllers/HelloWorldController.h"
+#include "app/controllers/EncryptController.h"
 
 using namespace Routes;
 using namespace App::Controllers;
@@ -11,4 +12,5 @@ void routes()
     Router::get()->addRoute<GradesController>("grade-average-input",
                                               &App::Controllers::GradesController::calculateAverageUsingInputs);
     Router::get()->addRoute<HelloWorldController>("", &App::Controllers::HelloWorldController::hello);
+    Router::get()->addRoute<EncryptController>("encrypt", &App::Controllers::EncryptController::encrypt);
 }
